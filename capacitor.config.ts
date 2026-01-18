0254ca0890220841542b804289621b1e2db23739
Jamie Ruderman <jamieruderman@gmail.com>
2026-01-18 13:24:31 -0800

    chore: update app display name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "smart.trains.schedule",
-  appName: "SMART Train Schedule",
+  appName: "SMART trip",
   webDir: "dist",
   plugins: {
     Keyboard: {

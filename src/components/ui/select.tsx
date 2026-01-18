d61ad473096cc9dadeed68d69d47222cfa75774a
Jamie Ruderman <jamieruderman@gmail.com>
2026-01-18 10:37:38 -0800

    style: bigger touch targets on mobile select

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -17,7 +17,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-12 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       // Add touch-action to eliminate 300ms delay on mobile
       "touch-none select-none",
       className
@@ -122,7 +122,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-pointer items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none hover:bg-amber-50 hover:text-amber-900 data-[highlighted]:bg-amber-50 data-[highlighted]:text-amber-900",
+      "relative flex w-full cursor-pointer items-center rounded-sm py-3 sm:py-1.5 pl-8 pr-2 text-sm outline-none hover:bg-amber-50 data-[highlighted]:bg-amber-50",
       className
     )}
     {...props}

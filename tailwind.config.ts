bc3b646b474b40d9c48ad58282ab428b812b32da
Jamie Ruderman <jamieruderman@gmail.com>
2026-03-09 09:35:19 -0700

    fix(theme): set smart-neutral to hsl(1, 0%, 45%)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,10 +19,22 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif'],
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+        ],
       },
       fontSize: {
-        '2xl': ['22px', { lineHeight: '1.2' }],
+        "2xl": ["22px", { lineHeight: "1.2" }],
       },
       colors: {
         border: "hsl(var(--border))",
@@ -76,7 +88,7 @@ export default {
           gold: "hsl(var(--smart-gold))" /* #E48E25 */,
           "dark-green": "hsl(var(--smart-dark-green))",
           "light-green": "hsl(var(--smart-light-green))",
-          neutral: "hsl(228deg 2.65% 62.94%)" /* default sheet header */,
+          neutral: "hsl(228deg 2.65% 45%)" /* default sheet header */,
         },
         // Transit-specific colors
         transit: {

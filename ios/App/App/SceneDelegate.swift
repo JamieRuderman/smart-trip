9bb3724147ef971f3e3d393ae8188c935f77b476
Jamie Ruderman <jamieruderman@gmail.com>
2026-09-25 11:23:24 -0700

    chore(deps): update Capacitor to 8.5.2 and route scenes through SceneDelegateProxy

--- ios/App/App/SceneDelegate.swift
+++ ios/App/App/SceneDelegate.swift
@@ -6,21 +6,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        if let url = connectionOptions.urlContexts.first?.url {
-            _ = ApplicationDelegateProxy.shared.application(UIApplication.shared, open: url)
-        }
-        if let userActivity = connectionOptions.userActivities.first {
-            _ = ApplicationDelegateProxy.shared.application(UIApplication.shared, continue: userActivity) { _ in }
-        }
+        SceneDelegateProxy.shared.scene(scene, willConnectTo: session, options: connectionOptions)
     }
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        guard let url = URLContexts.first?.url else { return }
-        _ = ApplicationDelegateProxy.shared.application(UIApplication.shared, open: url)
+        SceneDelegateProxy.shared.scene(scene, openURLContexts: URLContexts)
     }
 
     func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
-        _ = ApplicationDelegateProxy.shared.application(UIApplication.shared, continue: userActivity) { _ in }
+        SceneDelegateProxy.shared.scene(scene, continue: userActivity)
     }
 
 }
